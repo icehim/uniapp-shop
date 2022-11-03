f0dfd059cc17577c44653cf39857b0743861c973
zhiming <zhiming.hao@qq.com>
2022-11-03 22:35:21 +0800

    网络请求封装

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import '@/utils/request'
 
 Vue.config.productionTip = false
 
---
+++ src/utils/request.js
@@ -0,0 +1,31 @@
+const BASE_URL = 'https://api-hmugo-web.itheima.net/api/public/v1/'
+
+/*
+* 接收对象作为参数，返回Promise对象
+* */
+const request = ({url, method = 'GET', header = {}, data = {}, tip = '拼命加载中...'}) => {
+    return new Promise((resolve, reject) => {
+        uni.showLoading({
+            title: tip,
+            mask: true
+        })
+        uni.request({
+            url: `${BASE_URL}${url}`,
+            method,
+            data,
+            header,
+            success: res => {
+                resolve(res.data)
+            },
+            fail: err => {
+                reject(err)
+            },
+            complete: () => {
+                uni.hideLoading()
+            }
+        })
+    })
+}
+
+//挂载到全局的uni对象上去，这样每个页面都鞥你直接通过uni对象访问
+uni.$request = request
