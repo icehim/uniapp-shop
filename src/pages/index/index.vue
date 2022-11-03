5cc5500d455f9d9afeb4ffe5cc64445174b0b23d
zhiming <zhiming.hao@qq.com>
2022-11-03 21:50:04 +0800

    项目初始化

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,49 +1,54 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <!--uni app中不要写div、span 要写的话只能写uniapp提供的组件-->
+  <!--指令要用vue的-->
+  <view class="content">
+    <image class="logo" src="/static/logo.png"></image>
+    <view>
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+export default {
+  data() {
+    return {
+      title: 'Hello'
+    }
+  },
+  //生命周期需要使用uniapp提供的
+  onLoad() {
 
-		},
-		methods: {
-
-		}
-	}
+  },
+  methods: {}
+}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang="scss" scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
+}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin: 200rpx auto 50rpx auto;
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+
+}
 </style>
