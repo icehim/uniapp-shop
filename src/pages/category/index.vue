f1ffb8bf8eba951a323aada417008306c83c8a51
zhiming <zhiming.hao@qq.com>
2022-11-04 15:05:03 +0800

    分类一级菜单渲染完成

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -1,15 +1,103 @@
 <template>
-  <view>
-    <text>
-      分类
-    </text>
+  <view class="scroll-view-container">
+    <!--  左边的可以滚动的scroll-view -->
+    <scroll-view scroll-y class="left-scroll-view">
+      <view @click="currentIndex = index" v-for="(item,index) in categories" :key="item.cat_id"
+            :class="['left-scroll-view-item',currentIndex === index ?'active':'']">
+        {{ item.cat_name }}
+      </view>
+    </scroll-view>
   </view>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      categories: [],// 一级、二级、三级分类数组
+      currentIndex: 0  //选中的索引
+    }
+  },
+  onLoad() {
+    this.getCategoriesData()
+  },
+  methods: {
+    async getCategoriesData() {
+      const {meta: {status}, message} = await uni.$request({
+        url: 'categories'
+      })
+      if (status === 200) {
+        this.categories = message
+      }
+    }
+  },
+}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.scroll-view-container {
+  display: flex;
+
+  .left-scroll-view {
+    width: 240rpx;
+
+    .left-scroll-view-item {
+      line-height: 60px;
+      background-color: #f7f7f7;
+      text-align: center;
+      font-size: 12px;
+
+      // 激活项的样式
+      &.active {
+        background-color: #ffffff;
+        position: relative;
+
+        // 渲染激活项左侧的红色指示边线
+        &::before {
+          content: ' ';
+          display: block;
+          width: 3px;
+          height: 30px;
+          background-color: #c00000;
+          position: absolute;
+          left: 0;
+          top: 50%;
+          transform: translateY(-50%);
+        }
+      }
+    }
+  }
+
+  .right-scroll-view {
+    .cate-lv2-title {
+      font-size: 12px;
+      font-weight: bold;
+      text-align: center;
+      padding: 15px 0;
+    }
+
+    .cate-lv3-list {
+      display: flex;
+      flex-wrap: wrap;
+
+      .cate-lv3-item {
+        width: 33.33%;
+        margin-bottom: 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        image {
+          width: 60px;
+          height: 60px;
+        }
+
+        text {
+          font-size: 12px;
+        }
+      }
+    }
+  }
+}
 
 </style>
