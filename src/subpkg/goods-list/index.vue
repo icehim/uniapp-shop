5e5c122d9fc5cc760b082456159c10d80b1ea217
zhiming <zhiming.hao@qq.com>
2022-11-05 15:03:49 +0800

    上拉加载完成

--- src/subpkg/goods-list/index.vue
+++ src/subpkg/goods-list/index.vue
@@ -15,7 +15,8 @@ export default {
         pagesize: 10//每页也容量
       },
       goodsList: [],// 商品列表
-      total: 0  // 总条数
+      total: 0,  // 总条数
+      isLoadingMore: false//是否正在进行上拉加载
     }
   },
   onLoad(options) {
@@ -23,15 +24,31 @@ export default {
     this.queryObj.cid = options.cid || ''
     this.getGoodListData()
   },
+  onReachBottom() {
+    if (this.isLoadingMore) return;
+    //判断是否还有下一页数据，如果有则继续执行下面的代码，如果没有则提示并且return
+    if (this.goodsList.length >= this.total) {
+      uni.showToast({
+        title: '没有更多数据啦~',
+        duration: 800
+      })
+      return
+    }
+    //发请求
+    this.queryObj.pagenum++
+    this.getGoodListData()
+  },
   methods: {
     async getGoodListData() {
+      this.isLoadingMore = true
       const {meta: {status}, message: {goods, total}} = await uni.$request({
         url: 'goods/search',
         data: this.queryObj
       })
       if (status === 200) {
-        this.goodsList = goods
+        this.goodsList = [...this.goodsList, ...goods]
         this.total = total
+        this.isLoadingMore = false
       }
     }
   }
