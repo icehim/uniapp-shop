f354e1f90b0c9befea2293f6ca72edc2f3682d89
zhiming <zhiming.hao@qq.com>
2022-11-07 20:19:32 +0800

    点击结算之后的逻辑判断

--- src/components/my-settle/my-settle.vue
+++ src/components/my-settle/my-settle.vue
@@ -8,7 +8,7 @@
       合计:
       <text class="amount">￥{{ getSelectedAmount }}</text>
     </view>
-    <view class="btn-settle">
+    <view class="btn-settle" :style="{'background-color':getSelectedTotal > 0 ? '#c00000' : '#bdbdbd'}" @click="settle">
       结算({{ getSelectedTotal }})
     </view>
   </view>
@@ -18,18 +18,79 @@
 import {mapGetters, mapMutations} from "vuex";
 
 export default {
+  data() {
+    return {
+      time: 3, //3秒倒计时
+      timer: null //定时器
+    }
+  },
   name: "my-settle",
   computed: {
     ...mapGetters('cart', ['getTotal', 'getSelectedTotal', 'getSelectedAmount']),
+    ...mapGetters('user', ['getAddress', 'getToken']),
     isAllCheck() {
       return this.getTotal === this.getSelectedTotal
     }
   },
   methods: {
     ...mapMutations('cart', ['updateAllGoodsState']),
+    ...mapMutations('user', ['setRedirectInfo']),
     //全选或者反选
     toggleSelect() {
       this.updateAllGoodsState(!this.isAllCheck)
+    },
+    // 结算
+    settle() {
+      //要选择商品
+      if (this.getSelectedTotal === 0) {
+        uni.showToast({
+          title: '请选择商品进行结算',
+          icon: 'error',
+          duration: 800
+        })
+        return
+      }
+      // 要选择收获地址
+      if (!this.getAddress) {
+        uni.showToast({
+          title: '请选择收获地址',
+          icon: 'error',
+          duration: 800
+        })
+        return
+      }
+      // 判断是否登录
+      if (!this.getToken) {
+        uni.showToast({
+          title: '请登录后再结算!【' + this.time + '】秒之后跳转登录页',
+          icon: 'none',//如果文字太长，必须把icon设置为none
+          duration: 1000
+        })
+        // 定时器
+        this.timer = setInterval(() => {
+          if (this.time <= 1) {
+            clearInterval(this.timer)
+            this.time = 3
+
+            //进行跳转到我的页面
+            uni.switchTab({
+              url: '/pages/my/index',
+              success: () => {
+                //记录跳转来源页面
+                this.setRedirectInfo('/pages/cart/index')
+              }
+            })
+            return
+          }
+          this.time--
+          uni.showToast({
+            title: '请登录后再结算!【' + this.time + '】秒之后跳转登录页',
+            icon: 'none',//如果文字太长，必须把icon设置为none
+            duration: 1000
+          })
+        }, 1000)
+      }
+      // 下单和结算
     }
   },
 }
@@ -62,7 +123,7 @@ export default {
   .btn-settle {
     height: 50px;
     min-width: 100px;
-    background-color: #c00000;
+    //background-color: #c00000;
     color: white;
     line-height: 50px;
     text-align: center;
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,7 +1,9 @@
 export default {
     namespaced: true,
     state: {
-        address: uni.getStorageSync('address')
+        address: uni.getStorageSync('address'),//收货地址
+        token: uni.getStorageSync('token'),//token
+        redirectInfo: null//记录登陆成功之后，跳转到哪里去
     },
     getters: {
         getAddress(state) {
@@ -9,14 +11,29 @@ export default {
         },
         addressStr(state) {
             return state.address && `${state.address.provinceName}${state.address.cityName}${state.address.countyName}${state.address.detailInfo}`
+        },
+        getToken(state) {
+            return state.token
+        },
+        getRedirectInfo(stete) {
+            return stete.redirectInfo
         }
     },
     mutations: {
         setAddress(state, address) {
             state.address = address
-
             // 保存在本地
             uni.setStorageSync('address', address)
+        },
+        setToken(state, token) {
+            state.token = token
+            // 保存在本地
+            uni.setStorageSync('token', token)
+        },
+        setRedirectInfo(state, redirectInfo) {
+            state.redirectInfo = redirectInfo
+            // 保存在本地
+            uni.setStorageSync('redirectInfo', redirectInfo)
         }
     }
 }
